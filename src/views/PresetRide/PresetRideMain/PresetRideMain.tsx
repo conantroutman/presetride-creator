4ba785db4c53c39e583235c3deae2a57fb95b82f
conantroutman <johsod96@gmail.com>
2022-09-01 22:27:55 +0200

    Download all button

--- src/views/PresetRide/PresetRideMain/PresetRideMain.tsx
+++ src/views/PresetRide/PresetRideMain/PresetRideMain.tsx
@@ -1,10 +1,18 @@
+import { Affix, Button } from '@mantine/core';
 import React, { useEffect } from 'react';
+import { Download } from 'tabler-icons-react';
 import { PresetRideProvider } from '../../../contexts/PresetRideProvider';
 import useStore from '../../../store';
 import NavigationTabs from '../NavigationTabs/NavigationTabs';
+import DownloadAllButton from './components/DownloadAllButton';
 
 const PresetRideMain = () => {
-	return <NavigationTabs />;
+	return (
+		<>
+			<NavigationTabs />
+			<DownloadAllButton />
+		</>
+	);
 };
 
 export default PresetRideMain;
