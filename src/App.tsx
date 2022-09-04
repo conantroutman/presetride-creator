4308a620465a9f95804c9d6f98649303e0e9eb2b
conantroutman <johsod96@gmail.com>
2022-09-04 17:56:48 +0200

    React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,43 @@
-import { AppShell, Header, Navbar, NavLink, Tabs } from '@mantine/core';
-import { Plus } from 'tabler-icons-react';
+import { AppShell, Header } from '@mantine/core';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffectOnce } from 'usehooks-ts';
 import './App.css';
 import Navigation from './components/Navigation';
+import CustomCars from './views/CustomCars';
 import PresetRideMain from './views/PresetRide/PresetRideMain/PresetRideMain';
 
 function App() {
 	return (
-		<AppShell
-			padding="md"
-			navbar={<Navigation />}
-			header={
-				<Header height={60} p="xs">
-					Header
-				</Header>
-			}
-			styles={(theme) => ({
-				main: {
-					backgroundColor:
-						theme.colorScheme === 'dark'
-							? theme.colors.dark[8]
-							: theme.colors.gray[0],
-				},
-			})}
-		>
-			<PresetRideMain />
-		</AppShell>
+		<BrowserRouter>
+			<AppShell
+				padding="md"
+				navbar={<Navigation />}
+				header={
+					<Header height={60} p="xs">
+						Header
+					</Header>
+				}
+				styles={(theme) => ({
+					main: {
+						backgroundColor:
+							theme.colorScheme === 'dark'
+								? theme.colors.dark[8]
+								: theme.colors.gray[0],
+					},
+				})}
+			>
+				<Routes>
+					<Route
+						path="/"
+						element={<Navigate to="/most-wanted/presetride-editor" />}
+					/>
+					<Route path="most-wanted">
+						<Route path="presetride-editor" element={<PresetRideMain />} />
+						<Route path="custom-cars" element={<CustomCars />} />
+					</Route>
+				</Routes>
+			</AppShell>
+		</BrowserRouter>
 	);
 }
 
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,12 +1,28 @@
 import { Navbar, NavLink } from '@mantine/core';
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
 
 function Navigation() {
+	const location = useLocation();
+
 	return (
 		<Navbar width={{ base: 300 }} height={500} p="xs">
-			<NavLink label="Most Wanted">
-				<NavLink label="PresetRides" />
-				<NavLink label="Custom Cars" />
+			<NavLink
+				label="Most Wanted"
+				opened={location.pathname.includes('most-wanted')}
+			>
+				<NavLink
+					label="PresetRides"
+					component={Link}
+					to="/most-wanted/presetride-editor"
+					active={location.pathname === '/most-wanted/presetride-editor'}
+				/>
+				<NavLink
+					label="Custom Cars"
+					component={Link}
+					to="/most-wanted/custom-cars"
+					active={location.pathname === '/most-wanted/custom-cars'}
+				/>
 			</NavLink>
 			<NavLink label="Underground 2">
 				<NavLink label="PresetRides" />
