d9a93610b299fecbb91b71ffbff162f06cf72b15
conantroutman <johsod96@gmail.com>
2022-09-04 17:57:27 +0200

    Path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
 import { defineConfig } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
-  build: {
-    outDir: 'build',
-  },
-  plugins: [
-    react(),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-        // ...svgr options (https://react-svgr.com/docs/options/)
-      },
-    }),
-  ],
+	// This changes the out put dir from dist to build
+	// comment this out if that isn't relevant for your project
+	build: {
+		outDir: 'build',
+	},
+	plugins: [
+		react(),
+		svgrPlugin({
+			svgrOptions: {
+				icon: true,
+				// ...svgr options (https://react-svgr.com/docs/options/)
+			},
+		}),
+	],
+	resolve: {
+		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+	},
 });
