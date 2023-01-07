32dad061e4e5b91ad614317c0f4ff79bc1f75b85
John Söderberg <38869877+conantroutman@users.noreply.github.com>
2023-01-07 16:53:00 +0100

    Get rid of old context provider

--- src/hooks/usePartsData.ts
+++ src/hooks/usePartsData.ts
@@ -1,77 +1,78 @@
 import React, { useContext, useEffect, useMemo } from 'react';
-import { PresetRideContext } from '../contexts/PresetRideProvider';
 import { ICarMW } from '../games/mostwanted/cars';
 import { WindshieldTint } from '../games/mostwanted/colors';
 import { Hoods, RoofScoops, Spoilers, Wheels } from '../games/mostwanted/parts';
 import { decals } from '../games/mostwanted/visuals';
 
 const usePartsData = () => {
-  const { car } = useContext(PresetRideContext);
+	const bodyKits = useMemo(() => {
+		const bodyKitCount = 5;
+		const content: string[] = ['Stock'];
 
-  const bodyKits = useMemo(() => {
-    const bodyKitCount = car?.data?.bodyKits !== undefined ? car?.data?.bodyKits : 5;
-    const content: string[] = ['Stock'];
+		for (let index = 0; index < bodyKitCount; index++) {
+			content.push(`Body Kit 0${index + 1}`);
+		}
 
-    for (let index = 0; index < bodyKitCount; index++) {
-      content.push(`Body Kit 0${index + 1}`);
-    }
+		return content.map((bodyKit, index) => {
+			return { value: index.toString(), label: bodyKit };
+		});
+	}, []);
 
-    return content.map((bodyKit, index) => {
-      return { value: index.toString(), label: bodyKit };
-    });
-  }, [car]);
+	const hoods = useMemo(() => {
+		const data = Hoods.map((hood) => {
+			return { value: hood.id.toString(), label: hood.name };
+		});
 
-  const hoods = useMemo(() => {
-    const data = Hoods.map((hood) => {
-      return { value: hood.id.toString(), label: hood.name };
-    });
+		return [{ value: '0', label: 'Stock' }, ...data];
+	}, []);
 
-    return [{ value: '0', label: 'Stock' }, ...data];
-  }, [car]);
+	const roofScoops = useMemo(() => {
+		const data = RoofScoops.map((roofScoop) => {
+			return { value: roofScoop.id.toString(), label: roofScoop.name };
+		});
 
-  const roofScoops = useMemo(() => {
-    const data = RoofScoops.map((roofScoop) => {
-      return { value: roofScoop.id.toString(), label: roofScoop.name };
-    });
+		return [{ value: '0', label: 'Stock' }, ...data];
+	}, []);
 
-    return [{ value: '0', label: 'Stock' }, ...data];
-  }, [car]);
+	const windshieldTint = useMemo(() => {
+		const data = WindshieldTint.map((tint) => {
+			return { value: tint.id.toString(), label: tint.name };
+		});
 
-  const windshieldTint = useMemo(() => {
-    const data = WindshieldTint.map((tint) => {
-      return { value: tint.id.toString(), label: tint.name };
-    });
+		return [{ value: 'WINDOW_TINT_STOCK', label: 'Stock' }, ...data];
+	}, []);
 
-    return [{ value: 'WINDOW_TINT_STOCK', label: 'Stock' }, ...data];
-  }, [car]);
+	const rims = useMemo(() => {
+		const data = Wheels.map((brand) => {
+			return brand.rims.map((rim) => {
+				return {
+					value: `${brand.id}_STYLE0${rim.style}`,
+					label: rim.name,
+					group: brand.name,
+				};
+			});
+		});
+		const combinedData = new Array().concat(...data);
 
-  const rims = useMemo(() => {
-    const data = Wheels.map((brand) => {
-      return brand.rims.map((rim) => {
-        return { value: `${brand.id}_STYLE0${rim.style}`, label: rim.name, group: brand.name };
-      });
-    });
-    const combinedData = new Array().concat(...data);
+		return [{ value: '0', label: 'Stock', group: 'Stock' }, ...combinedData];
+	}, []);
 
-    return [{ value: '0', label: 'Stock', group: 'Stock' }, ...combinedData];
-  }, []);
+	const decalsData = useMemo(() => {
+		const data = decals.map((decal) => {
+			return { value: decal.id.toString(), label: decal.name };
+		});
 
-  const decalsData = useMemo(() => {
-    const data = decals.map((decal) => {
-      return { value: decal.id.toString(), label: decal.name };
-    });
+		return [{ value: '0', label: 'Stock' }, ...data];
+	}, []);
 
-    return [{ value: '0', label: 'Stock' }, ...data];
-  }, []);
-
-  return {
-    bodyKits,
-    hoods,
-    roofScoops,
-    windshieldTint,
-    rims,
-    decalsData,
-  };
+	return {
+		bodyKits,
+		rims,
+		decalsData,
+		hoods,
+		roofScoops,
+		windshieldTint,
+	};
 };
 
 export default usePartsData;
