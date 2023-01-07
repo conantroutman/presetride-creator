b4c534716f03197bbe881f4948c310183623bc39
John Söderberg <38869877+conantroutman@users.noreply.github.com>
2023-01-07 17:19:46 +0100

    Formatting

--- src/views/PresetRide/Form/FormProvider.tsx
+++ src/views/PresetRide/Form/FormProvider.tsx
@@ -4,30 +4,32 @@ import useStore from '../../../store';
 import { IFormValues } from './interfaces';
 
 interface IFormContext {
-  id: string;
-  form: UseFormReturnType<any>;
+	id: string;
+	form: UseFormReturnType<any>;
 }
 
 export const FormContext = React.createContext({} as IFormContext);
 
 const FormProvider = (props: { children: ReactNode; id: string }) => {
-  const { children, id } = props;
-  const presetRide = useStore((state) => state.presetRides.find((element) => element.id === id));
+	const { children, id } = props;
+	const presetRide = useStore((state) =>
+		state.presetRides.find((element) => element.id === id)
+	);
 
-  const form = useForm<IFormValues>({
-    initialValues: presetRide,
-  });
+	const form = useForm<IFormValues>({
+		initialValues: presetRide,
+	});
 
-  return (
-    <FormContext.Provider
-      value={{
-        id,
-        form,
-      }}
-    >
-      {children}
-    </FormContext.Provider>
-  );
+	return (
+		<FormContext.Provider
+			value={{
+				id,
+				form,
+			}}
+		>
+			{children}
+		</FormContext.Provider>
+	);
 };
 
 export default FormProvider;
