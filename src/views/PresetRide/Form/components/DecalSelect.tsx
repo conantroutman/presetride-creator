9a4e63873cdfd96331bfd3f3b4f6d56219f212d8
John Söderberg <38869877+conantroutman@users.noreply.github.com>
2023-01-07 16:53:11 +0100

    Fixed imports

--- src/views/PresetRide/Form/components/DecalSelect.tsx
+++ src/views/PresetRide/Form/components/DecalSelect.tsx
@@ -1,36 +1,35 @@
-import { Group, SegmentedControl, Select } from '@mantine/core';
-import React, { useContext } from 'react';
-import usePartsData from '../../../../hooks/usePartsData';
+import usePartsData from '@/hooks/usePartsData';
+import { Select } from '@mantine/core';
+import { useContext } from 'react';
 import { FormContext } from '../FormProvider';
-import { IFormValues } from '../interfaces';
 
 const DecalSelect = (props: { formInputName: string; label?: string }) => {
-  const { formInputName, label } = props;
-  const { form } = useContext(FormContext);
-  const { decalsData } = usePartsData();
+	const { formInputName, label } = props;
+	const { form } = useContext(FormContext);
+	const { decalsData } = usePartsData();
 
-  const colorSelect = (
-    <Select
-      data={[
-        { label: 'Black', value: 'BLACK' },
-        { label: 'White', value: 'WHITE' },
-      ]}
-      defaultValue="BLACK"
-      {...form.getInputProps(`decals.${formInputName}.color`)}
-      width={92}
-    />
-  );
+	const colorSelect = (
+		<Select
+			data={[
+				{ label: 'Black', value: 'BLACK' },
+				{ label: 'White', value: 'WHITE' },
+			]}
+			defaultValue="BLACK"
+			{...form.getInputProps(`decals.${formInputName}.color`)}
+			width={92}
+		/>
+	);
 
-  return (
-    <Select
-      label={label}
-      defaultValue="0"
-      data={decalsData}
-      {...form.getInputProps(`decals.${formInputName}.name`)}
-      rightSection={colorSelect}
-      rightSectionWidth={92}
-    />
-  );
+	return (
+		<Select
+			label={label}
+			defaultValue="0"
+			data={decalsData}
+			{...form.getInputProps(`decals.${formInputName}.name`)}
+			rightSection={colorSelect}
+			rightSectionWidth={92}
+		/>
+	);
 };
 
 export default DecalSelect;
